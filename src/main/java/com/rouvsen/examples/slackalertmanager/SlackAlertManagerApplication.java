b1a87d6b980487f99f263d83032291eb7d375c0a
rovshan.maharramov <rovshan.maharramov@guavapay.com>
2025-02-09 21:41:29 +0400

    initial commit

---
+++ src/main/java/com/rouvsen/examples/slackalertmanager/SlackAlertManagerApplication.java
@@ -0,0 +1,13 @@
+package com.rouvsen.examples.slackalertmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlackAlertManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SlackAlertManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rouvsen/examples/slackalertmanager/SlackAlertManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rouvsen.examples.slackalertmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlackAlertManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
